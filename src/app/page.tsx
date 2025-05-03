92a84418b8ab7ed6eabc0230570f012dfddff805
imdhruv99 <dpdhruvprajapati@gmail.com>
2025-05-03 22:19:24 +0530

    [UPDATED] Home to Main and others

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+import Navbar from '../components/Navbar';
+
+export default function Main() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1>Hello World</h1>
+        <Navbar />
     </div>
   );
 }
