23b35f0cede08da5cc44bcd04be003617ba84270
Dhruv Prajapati <dprajapati@juniper.net>
2025-06-16 12:51:34 +0530

    [ADDED] code clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 'use client';
 
-import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 import Home from '@/components/Home';
 
 export default function ProjectsPage() {
-    const { theme, resolvedTheme } = useTheme();
     const [isMounted, setIsMounted] = useState(false);
-    const isDarkTheme =
-        theme === 'dark' || (theme === 'system' && resolvedTheme === 'dark');
 
     useEffect(() => {
         setIsMounted(true);
@@ -18,5 +14,5 @@ export default function ProjectsPage() {
         return null;
     }
 
-    return <Home isDarkTheme={isDarkTheme} />;
+    return <Home />;
 }
